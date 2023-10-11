6043747749a4dc8dc1517c09aaf9c016a08d13f6
princeemtiaz <111088803+princeemtiaz@users.noreply.github.com>
2023-10-11 10:44:31 +0600

    CLASSWORK IS DONE

--- ASSIGNMENT-02/CLASSWORK/6.py
+++ ASSIGNMENT-02/CLASSWORK/6.py
@@ -7,20 +7,18 @@ def correction(string=input('Enter a string: ')):
     
   
     for i in range(1, len(string)):
-        # Check if the current character is 'i' and is surrounded by spaces.
+        # Check if the current character is 'i' and  spaces.
         if (string[i - 1] == ' ') and (string[i + 1] == ' ') and string[i] == 'i':
             # Capitalize 'i' to 'I' in this case.
             s += 'I'
         else:
             # Check if the character before the current character is a space and the one before that is a punctuation mark.
-            if (string[i - 1] == ' ') and (string[i - 2] in sp):
-                # Capitalize the current character, assuming it's the start of a sentence or line.
+            if (string[i - 1] == ' ') and (string[i - 2] in sp):  
+            
                 s += string[i].upper()
             else:
                 
                 s += string[i]
-    
-    # Return the corrected string 's'.
     return s
 
 
