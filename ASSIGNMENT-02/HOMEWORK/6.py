638453ff7cd57e433600ac7abbed57bea05501af
princeemtiaz <138528626+prince-efty@users.noreply.github.com>
2023-10-17 22:52:08 +0600

    modify six

--- ASSIGNMENT-02/HOMEWORK/6.py
+++ ASSIGNMENT-02/HOMEWORK/6.py
@@ -1,44 +1,38 @@
 import random
+def playRockPaperScissor(rounds) :
+    human_score = 0
+    machine_score = 0
+    for i in range(rounds) :
+        hum = input('Your turn: ')
+        mec = random.choice(['Rock','Paper','Scissor'])
+        print('Computer:',mec)
 
-def playRockPaperScissor(rounds):
-    player_score = 0
-    computer_score = 0
-    choices = ["rock", "paper", "scissor"]
+        if hum == 'Rock' and mec == 'Paper' :
+            machine_score += 1
+        elif mec == 'Rock' and hum == 'Paper' :
+            human_score += 1
+        elif hum == 'Rock' and mec == 'Scissor' :
+            human_score += 1
+        elif hum == 'Scissor' and mec == 'Rock' :
+            machine_score += 1
 
-    for round in range(1, rounds + 1):
-        print(f"Round {round}:")
 
-        player_choice = input("Your choice (rock/paper/scissor): ").lower()
-        if player_choice not in choices:
-            print("Invalid input. Please choose rock, paper, or scissor.")
-            continue
+        elif hum == 'Paper' and mec == 'Scissor' :
+            machine_score += 1
+        elif hum == 'Scissor' and mec == 'Paper' :
+            human_score += 1
 
-        computer_choice = random.choice(choices)
-        print(f"Computer: {computer_choice}")
+        elif hum == mec :
+            human_score += 0
+            machine_score += 0
+    print(f'Your score: {human_score}')
+    print(f"Computer's score: {machine_score}")
 
-        if player_choice == computer_choice:
-            print("It's a tie!")
-        elif (
-            (player_choice == "rock" and computer_choice == "scissor") or
-            (player_choice == "scissor" and computer_choice == "paper") or
-            (player_choice == "paper" and computer_choice == "rock")
-        ):
-            print("You win this round!")
-            player_score += 1
-        else:
-            print("Computer wins this round!")
-            computer_score += 1
+    if machine_score > human_score :
+        print('Computer has won the game!')
+    elif machine_score < human_score :
+        print('You have won the game!')
+    else :
+        print('Tied!')
 
-    print(f"Your Score: {player_score}")
-    print(f"Computer's Score: {computer_score}")
-
-    if player_score > computer_score:
-        print("You have won the game!")
-    elif player_score < computer_score:
-        print("Computer has won the game!")
-    else:
-        print("It's a tie!")
-
-# Example usage:
-rounds = int(input("Enter the number of rounds: "))
-playRockPaperScissor(rounds)
+playRockPaperScissor(3)
