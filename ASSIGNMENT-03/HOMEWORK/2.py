f3dde588bbfb529426f47fe4b44f006d9be26fc6
princeemtiaz <138528626+prince-efty@users.noreply.github.com>
2023-10-26 01:27:43 +0600

    update 2

--- ASSIGNMENT-03/HOMEWORK/2.py
+++ ASSIGNMENT-03/HOMEWORK/2.py
@@ -1,27 +1,28 @@
 class box:
-    def __init__(self, length, width, height):
+    def __init__(self,nums):
+        self.height=nums[0]
+        self.width=nums[1]
+        self.breadth=nums[2]
         print("Creating a Box!")
-        self.length = length
-        self.width = width
-        self.height = height
-
-    def volume(self):
-        return self.length * self.width * self.height
 
     def boxDescription(self):
         print("Height:", self.height)
         print("Width:", self.width)
-        print("Length:", self.length)
+        print("Breadth:", self.breadth)
+    def volume(self):
+        volume = self.height * self.width * self.breadth
+        return f"the volume of the box is {volume} cubic units"
 
-# Driver code
+
+# Write your class code here
 print("Box 1")
-b1 = box(10, 10, 10)  
+b1 = box([10,10,10])
 print("=========================")
 b1.boxDescription()
 print(b1.volume())
 print("-------------------------")
 print("Box 2")
-b2 = box(30, 10, 10)  
+b2 = box((30,10,10))
 print("=========================")
 b2.boxDescription()
 print(b2.volume())
@@ -29,11 +30,17 @@ b2.height = 300
 print("Updating Box 2!")
 print("Height:", b2.height)
 print("Width:", b2.width)
-print("Length:", b2.length)
-volume = b2.height * b2.width * b2.length
+print("Breadth:", b2.breadth)
+volume = b2.height * b2.width * b2.breadth
 print(f"Volume of the box is {volume} cubic units.")
 print("-------------------------")
 print("Box 3")
 b3 = b2
 b3.boxDescription()
 print(b3.volume())
+
+one = (b3 == b2)
+b3.width = 100
+two = (b3 == b2)
+print(one,two)
+print(b2.width)
